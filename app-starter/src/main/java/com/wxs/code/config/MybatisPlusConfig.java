2a284cac30d0017d6a92d64583b4aff3bfb2ec0e
xswang <wxs_code@126.com>
2024-03-16 15:40:24 +0800

    common模块添加分页插件

--- app-starter/src/main/java/com/wxs/code/config/MybatisPlusConfig.java
+++ app-starter/src/main/java/com/wxs/code/config/MybatisPlusConfig.java
@@ -1,11 +1,7 @@
 package com.wxs.code.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
-import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.LongValue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
