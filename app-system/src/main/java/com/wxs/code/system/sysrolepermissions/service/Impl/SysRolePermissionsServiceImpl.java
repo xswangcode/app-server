4a978251bee9676d42142c9352d9f7c545034e1b
xswangcode <2236785477@qq.co>
2025-02-14 14:25:52 +0800

    权限、权限角色、角色、角色用户关系修改时删除权限角色的缓存

--- app-system/src/main/java/com/wxs/code/system/sysrolepermissions/service/Impl/SysRolePermissionsServiceImpl.java
+++ app-system/src/main/java/com/wxs/code/system/sysrolepermissions/service/Impl/SysRolePermissionsServiceImpl.java
@@ -15,11 +15,14 @@ package com.wxs.code.system.sysrolepermissions.service.Impl;
 
 
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.wxs.code.constant.RedisConstants;
 import com.wxs.code.core.service.BaseService;
 import com.wxs.code.system.sysrolepermissions.entity.SysRolePermissions;
 import com.wxs.code.system.sysrolepermissions.service.ISysRolePermissionsService;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Service
@@ -33,5 +36,15 @@ public class SysRolePermissionsServiceImpl extends BaseService<SysRolePermission
     public List<SysRolePermissions> getByRoleIds(List<Long> roleIds) {
         List<SysRolePermissions> role_permissions = list(Wrappers.lambdaQuery(SysRolePermissions.class).in(SysRolePermissions::getRoleId, roleIds));
         return role_permissions;
+    }
+
+    /**
+     * @param id 主键ID
+     * @return
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    public boolean removeById(Serializable id) {
+        return super.removeById(id);
     }
 }
--- app-system/src/main/java/com/wxs/code/system/sysuserrole/service/ISysUserRoleService.java
+++ app-system/src/main/java/com/wxs/code/system/sysuserrole/service/ISysUserRoleService.java
@@ -13,14 +13,306 @@
 package com.wxs.code.system.sysuserrole.service;
 
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.extension.conditions.update.LambdaUpdateChainWrapper;
+import com.baomidou.mybatisplus.extension.conditions.update.UpdateChainWrapper;
+import com.baomidou.mybatisplus.extension.kotlin.KtUpdateChainWrapper;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.wxs.code.constant.RedisConstants;
 import com.wxs.code.system.sysuserrole.entity.SysUserRole;
+import org.springframework.cache.annotation.CacheEvict;
 
+import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 
 public interface ISysUserRoleService extends IService<SysUserRole> {
 
     List<SysUserRole> getByUserId(Long userId);
 
+    /**
+     * 批量修改插入
+     *
+     * @param entityList 实体对象集合
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean saveOrUpdateBatch(Collection<SysUserRole> entityList) {
+        return IService.super.saveOrUpdateBatch(entityList);
+    }
+
+    /**
+     * 批量修改插入
+     *
+     * @param entityList 实体对象集合
+     * @param batchSize  每次的数量
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    boolean saveOrUpdateBatch(Collection<SysUserRole> entityList, int batchSize);
+
+    /**
+     * 根据 ID 删除
+     *
+     * @param id 主键ID
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean removeById(Serializable id) {
+        return IService.super.removeById(id);
+    }
+
+    /**
+     * 根据 ID 删除
+     *
+     * @param id      主键(类型必须与实体类型字段保持一致)
+     * @param useFill 是否启用填充(为true的情况,会将入参转换实体进行delete删除)
+     * @return 删除结果
+     * @since 3.5.0
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean removeById(Serializable id, boolean useFill) {
+        return IService.super.removeById(id, useFill);
+    }
+
+    /**
+     * 根据实体(ID)删除
+     *
+     * @param entity 实体
+     * @since 3.4.4
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean removeById(SysUserRole entity) {
+        return IService.super.removeById(entity);
+    }
+
+    /**
+     * 根据 columnMap 条件，删除记录
+     *
+     * @param columnMap 表字段 map 对象
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean removeByMap(Map<String, Object> columnMap) {
+        return IService.super.removeByMap(columnMap);
+    }
+
+    /**
+     * 根据 entity 条件，删除记录
+     *
+     * @param queryWrapper 实体包装类 {@link QueryWrapper}
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean remove(Wrapper<SysUserRole> queryWrapper) {
+        return IService.super.remove(queryWrapper);
+    }
+
+    /**
+     * 删除（根据ID 批量删除）
+     *
+     * @param list 主键ID或实体列表
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean removeByIds(Collection<?> list) {
+        return IService.super.removeByIds(list);
+    }
+
+    /**
+     * 批量删除
+     *
+     * @param list    主键ID或实体列表
+     * @param useFill 是否填充(为true的情况,会将入参转换实体进行delete删除)
+     * @return 删除结果
+     * @since 3.5.0
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean removeByIds(Collection<?> list, boolean useFill) {
+        return IService.super.removeByIds(list, useFill);
+    }
+
+    /**
+     * 批量删除(jdbc批量提交)
+     *
+     * @param list 主键ID或实体列表(主键ID类型必须与实体类型字段保持一致)
+     * @return 删除结果
+     * @since 3.5.0
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean removeBatchByIds(Collection<?> list) {
+        return IService.super.removeBatchByIds(list);
+    }
+
+    /**
+     * 批量删除(jdbc批量提交)
+     *
+     * @param list    主键ID或实体列表(主键ID类型必须与实体类型字段保持一致)
+     * @param useFill 是否启用填充(为true的情况,会将入参转换实体进行delete删除)
+     * @return 删除结果
+     * @since 3.5.0
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean removeBatchByIds(Collection<?> list, boolean useFill) {
+        return IService.super.removeBatchByIds(list, useFill);
+    }
+
+    /**
+     * 批量删除(jdbc批量提交)
+     *
+     * @param list      主键ID或实体列表
+     * @param batchSize 批次大小
+     * @return 删除结果
+     * @since 3.5.0
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean removeBatchByIds(Collection<?> list, int batchSize) {
+        return IService.super.removeBatchByIds(list, batchSize);
+    }
+
+    /**
+     * 批量删除(jdbc批量提交)
+     *
+     * @param list      主键ID或实体列表
+     * @param batchSize 批次大小
+     * @param useFill   是否启用填充(为true的情况,会将入参转换实体进行delete删除)
+     * @return 删除结果
+     * @since 3.5.0
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean removeBatchByIds(Collection<?> list, int batchSize, boolean useFill) {
+        return IService.super.removeBatchByIds(list, batchSize, useFill);
+    }
+
+    /**
+     * 根据 ID 选择修改
+     *
+     * @param entity 实体对象
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean updateById(SysUserRole entity) {
+        return IService.super.updateById(entity);
+    }
+
+    /**
+     * 根据 UpdateWrapper 条件，更新记录 需要设置sqlset
+     *
+     * @param updateWrapper 实体对象封装操作类 {@link UpdateWrapper}
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean update(Wrapper<SysUserRole> updateWrapper) {
+        return IService.super.update(updateWrapper);
+    }
+
+    /**
+     * 根据 whereEntity 条件，更新记录
+     *
+     * @param entity        实体对象
+     * @param updateWrapper 实体对象封装操作类 {@link UpdateWrapper}
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean update(SysUserRole entity, Wrapper<SysUserRole> updateWrapper) {
+        return IService.super.update(entity, updateWrapper);
+    }
+
+    /**
+     * 根据ID 批量更新
+     *
+     * @param entityList 实体对象集合
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean updateBatchById(Collection<SysUserRole> entityList) {
+        return IService.super.updateBatchById(entityList);
+    }
+
+    /**
+     * 根据ID 批量更新
+     *
+     * @param entityList 实体对象集合
+     * @param batchSize  更新批次数量
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    boolean updateBatchById(Collection<SysUserRole> entityList, int batchSize);
+
+    /**
+     * TableId 注解存在更新记录，否插入一条记录
+     *
+     * @param entity 实体对象
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    boolean saveOrUpdate(SysUserRole entity);
+
+    /**
+     * 链式查询 lambda 式
+     * kotlin 使用
+     *
+     * @return KtQueryWrapper 的包装类
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default KtUpdateChainWrapper<SysUserRole> ktUpdate() {
+        return IService.super.ktUpdate();
+    }
+
+    /**
+     * 链式更改 普通
+     *
+     * @return UpdateWrapper 的包装类
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default UpdateChainWrapper<SysUserRole> update() {
+        return IService.super.update();
+    }
+
+    /**
+     * 链式更改 lambda 式
+     * <p>注意：不支持 Kotlin </p>
+     *
+     * @return LambdaUpdateWrapper 的包装类
+     */
+    @Override
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default LambdaUpdateChainWrapper<SysUserRole> lambdaUpdate() {
+        return IService.super.lambdaUpdate();
+    }
+
+    /**
+     * <p>
+     * 根据updateWrapper尝试更新，否继续执行saveOrUpdate(T)方法
+     * 此次修改主要是减少了此项业务代码的代码量（存在性验证之后的saveOrUpdate操作）
+     * </p>
+     * <p>
+     * 该方法不推荐在多线程并发下使用，并发可能存在间隙锁的问题，可以采用先查询后判断是否更新或保存。
+     * </p>
+     * <p>
+     * 该方法存在安全隐患将在后续大版本删除
+     * </p>
+     *
+     * @param entity        实体对象
+     * @param updateWrapper
+     */
+    @Override
+    @Deprecated
+    @CacheEvict(value = {RedisConstants.GET_PERMISSION_LIST_KEY, RedisConstants.GET_ROLE_LIST_KEY}, key = "#StpUtil.getLoginId() + ':' + #StpUtil.getLoginType()")
+    default boolean saveOrUpdate(SysUserRole entity, Wrapper<SysUserRole> updateWrapper) {
+        return IService.super.saveOrUpdate(entity, updateWrapper);
+    }
 }
